4c612de8cd3bd292e5e1d3524da3047a8be6f336
Md Hamimul Haque <mdhamimulhaque888@gmail.com>
2022-11-04 17:08:39 +0600

    genius car client

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOjWDvuk0qJdtjXxNQJpGwMn6dnMZJhto",
+  authDomain: "jenious-car-20c47.firebaseapp.com",
+  projectId: "jenious-car-20c47",
+  storageBucket: "jenious-car-20c47.appspot.com",
+  messagingSenderId: "763785933973",
+  appId: "1:763785933973:web:29f3565fd6f104de58d79c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
